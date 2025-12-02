5bf895ae24a763b1b629edb64da475dcada21639
Alex Zurek <alexander.j.zurek@gmail.com>
2025-12-02 13:07:13 -0500

    Day 1, part 1 and 2

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,10 +1,87 @@
 defmodule Day01.Part1 do
-  def solve(_input) do
+  def solve(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&parseRow/1)
+    |> adjustDial(50)
+  end
+
+  defp parseRow("R" <> tail) do
+    {res, _} = Integer.parse(tail)
+    res
+  end
+
+  defp parseRow("L" <> tail) do
+    {res, _} = Integer.parse(tail)
+    res * -1
+  end
+
+  defp adjustDial(adjustments, start) do
+    [num_zeros, _] =
+      Enum.reduce(adjustments, [0, start], fn adj, [num_zeros, pos] ->
+        pos_adj = pos + rem(adj, 100)
+
+        cond do
+          pos_adj < 0 -> [num_zeros, 100 + pos_adj]
+          pos_adj > 100 -> [num_zeros, pos_adj - 100]
+          pos_adj == 0 or pos_adj == 100 -> [num_zeros + 1, 0]
+          true -> [num_zeros, pos_adj]
+        end
+      end)
+
+    num_zeros
   end
 end
 
 defmodule Day01.Part2 do
-  def solve(_input) do
+  def solve(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&parseRow/1)
+    |> adjustDial(50)
+  end
+
+  defp parseRow("R" <> tail) do
+    {res, _} = Integer.parse(tail)
+    res
+  end
+
+  defp parseRow("L" <> tail) do
+    {res, _} = Integer.parse(tail)
+    res * -1
+  end
+
+  defp adjustDial(adjustments, start) do
+    [count, _] =
+      Enum.reduce(adjustments, [0, start], fn adj, [count, pos] ->
+        pos_adj = pos + rem(adj, 100)
+        num_passes = abs(div(adj, 100))
+
+        new_pos =
+          cond do
+            pos_adj < 0 -> 100 + pos_adj
+            pos_adj > 100 -> pos_adj - 100
+            pos_adj == 100 -> 0
+            true -> pos_adj
+          end
+
+        crossed_zero =
+          cond do
+            pos == 0 or new_pos == 0 -> false
+            adj > 0 && new_pos < pos -> true
+            adj < 0 && new_pos > pos -> true
+            true -> false
+          end
+
+        new_count =
+          cond do
+            new_pos == 0 && pos != 0 -> count + num_passes + 1
+            crossed_zero -> count + num_passes + 1
+            true -> count + num_passes
+          end
+
+        [new_count, new_pos]
+      end)
+
+    count
   end
 end
 
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -2,15 +2,57 @@ defmodule Day01Test do
   use ExUnit.Case
 
   @example_input """
+  L68
+  L30
+  R48
+  L5
+  R60
+  L55
+  L1
+  L99
+  R14
+  L82
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day01.part_1(@example_input) == 142
+    assert Day01.Part1.solve(@example_input) == 3
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day01.part_2(@example_input) == 281
+    assert Day01.Part2.solve(@example_input) == 6
+  end
+
+  test "solves many right rotations around the dial" do
+    input = """
+    R1000
+    """
+
+    assert Day01.Part2.solve(input) == 10
+  end
+
+  test "solves many left rotations around the dial" do
+    input = """
+    L1000
+    """
+
+    assert Day01.Part2.solve(input) == 10
+  end
+
+  test "handles small left adjustments" do
+    input = """
+    L48
+    L3
+    """
+
+    assert Day01.Part2.solve(input) == 1
+  end
+
+  test "handles small right adjustments" do
+    input = """
+    R48
+    R3
+    """
+
+    assert Day01.Part2.solve(input) == 1
   end
 end
