9b72873a44dd175e79b9658033790e1a80d6adb8
Alex Zurek <alexander.j.zurek@gmail.com>
2025-12-03 18:36:02 -0500

    Finished Day 2

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,12 +1,65 @@
 defmodule Day02.Part1 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    input
+    |> String.split(",", trim: true)
+    |> Enum.map(fn range ->
+      [start_str, final_str] = String.split(range, "-")
+      {start, _} = Integer.parse(start_str)
+      {final, _} = Integer.parse(final_str)
+      start..final
+    end)
+    |> Enum.reduce([], fn range, invalid_ids ->
+      invalid_in_range =
+        Enum.reduce(range, [], fn val, acc ->
+          str = Integer.to_string(val)
+          {first, second} = String.split_at(str, div(String.length(str), 2))
+
+          cond do
+            first == second -> [val | acc]
+            true -> acc
+          end
+        end)
+
+      [invalid_in_range | invalid_ids]
+    end)
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.sum()
   end
 end
 
 defmodule Day02.Part2 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    input
+    |> String.split(",", trim: true)
+    |> Enum.map(fn range ->
+      [start_str, final_str] = String.split(range, "-")
+      {start, _} = Integer.parse(start_str)
+      {final, _} = Integer.parse(final_str)
+      start..final
+    end)
+    |> Enum.reduce(0, fn range, total ->
+      total_for_range =
+        Enum.reduce(range, 0, fn val, acc ->
+          str = Integer.to_string(val)
+
+          invalid_id_sum =
+            0..div(String.length(str), 2)
+            |> Enum.reduce([], fn index, ids ->
+              sub_str = String.slice(str, 0, index)
+
+              cond do
+                Enum.all?(String.split(str, sub_str), fn x -> x == "" end) -> [val | ids]
+                true -> ids
+              end
+            end)
+            |> Enum.uniq()
+            |> Enum.sum()
+
+          acc + invalid_id_sum
+        end)
+
+      total + total_for_range
+    end)
   end
 end
 
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -2,15 +2,21 @@ defmodule Day02Test do
   use ExUnit.Case
 
   @example_input """
+  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day02.Part1.solve(@example_input) == 8
+    assert Day02.Part1.solve(@example_input) == 1227775554
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day02.Part2.solve(@example_input) == 2286
+    assert Day02.Part2.solve(@example_input) == 4174379265
+  end
+
+  test "part 2 small example" do
+    input = """
+    11-22
+    """
+    assert Day02.Part2.solve(input) == 33
   end
 end
