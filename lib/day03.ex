72f96bd9eea81c94a2a4d308169c2c4f208a81c8
Alex Zurek <alexander.j.zurek@gmail.com>
2025-12-05 10:23:25 -0500

    Finished Day 3

--- lib/day03.ex
+++ lib/day03.ex
@@ -1,12 +1,47 @@
 defmodule Day03.Part1 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn bank ->
+      batteries = String.split(bank, "", trim: true)
+
+      for i <- 0..(length(batteries) - 2),
+          j <- (i + 1)..(length(batteries) - 1) do
+        String.to_integer(Enum.at(batteries, i) <> Enum.at(batteries, j))
+      end
+      |> Enum.max()
+    end)
+    |> Enum.sum()
   end
 end
 
 defmodule Day03.Part2 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&process_bank_v2(&1, 0, 12))
+    |> Enum.map(fn val -> val |> Integer.parse() |> elem(0) end)
+    |> Enum.sum()
+  end
+
+  defp process_bank_v2(bank, index, 1) do
+    bank
+    |> String.slice(index..-1//1)
+    |> String.graphemes()
+    |> Enum.max()
+  end
+
+  defp process_bank_v2(bank, index, num_chars_left) do
+    bank_length = String.length(bank)
+    batteries = String.graphemes(bank)
+
+    {max_val, max_index} =
+      Enum.slice(batteries, index..(bank_length - 1))
+      |> Enum.with_index(index)
+      |> Enum.filter(fn {_, i} -> i + num_chars_left <= bank_length end)
+      |> Enum.max_by(fn {val, _} -> val end, fn -> {"0", index} end)
+
+    max_val <> process_bank_v2(bank, max_index + 1, num_chars_left - 1)
   end
 end
 
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -2,15 +2,27 @@ defmodule Day03Test do
   use ExUnit.Case
 
   @example_input """
+  987654321111111
+  811111111111119
+  234234234234278
+  818181911112111
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day03.Part1.solve(@example_input) == 8
+    assert Day03.Part1.solve("987654321111111") == 98
+    assert Day03.Part1.solve("811111111111119") == 89
+    assert Day03.Part1.solve("234234234234278") == 78
+    assert Day03.Part1.solve("818181911112111") == 92
+
+    assert Day03.Part1.solve(@example_input) == 357
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day03.Part2.solve(@example_input) == 2286
+    assert Day03.Part2.solve("987654321111111") == 987_654_321_111
+    assert Day03.Part2.solve("811111111111119") == 811_111_111_119
+    assert Day03.Part2.solve("234234234234278") == 434_234_234_278
+    assert Day03.Part2.solve("818181911112111") == 888_911_112_111
+
+    assert Day03.Part2.solve(@example_input) == 3_121_910_778_619
   end
 end
