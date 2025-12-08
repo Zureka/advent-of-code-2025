540df7e407d3d09a55c9bc5917ea1194bf4cce16
Alex Zurek <alexander.j.zurek@gmail.com>
2025-12-08 09:38:49 -0500

    Solved Day 5

--- lib/day05.ex
+++ lib/day05.ex
@@ -1,12 +1,69 @@
 defmodule Day05.Part1 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    [ranges, ids] = String.split(input, "\n\n", trim: true)
+
+    id_ranges =
+      ranges
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [start_str, end_str] = String.split(line, "-", trim: true)
+        {String.to_integer(start_str), String.to_integer(end_str)}
+      end)
+
+    ids
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn id_str, acc ->
+      id = String.to_integer(id_str)
+
+      cond do
+        Enum.any?(id_ranges, fn {id_start, id_end} ->
+          id >= id_start and id <= id_end
+        end) ->
+          acc + 1
+
+        true ->
+          acc
+      end
+    end)
   end
 end
 
 defmodule Day05.Part2 do
-  def solve(_input) do
-    0
+  def solve(input) do
+    [ranges, ids] = String.split(input, "\n\n", trim: true)
+
+    id_ranges =
+      ranges
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [start_str, end_str] = String.split(line, "-", trim: true)
+        {String.to_integer(start_str), String.to_integer(end_str)}
+      end)
+      |> Enum.sort()
+
+    merged_ranges = merge_ranges(id_ranges)
+
+    merged_ranges
+    |> Enum.map(fn {id_start, id_end} -> id_end - id_start + 1 end)
+    |> Enum.sum()
+  end
+
+  defp merge_ranges([]), do: []
+
+  defp merge_ranges([first | rest]) do
+    merge_ranges(rest, [first])
+  end
+
+  defp merge_ranges([], acc), do: Enum.reverse(acc)
+
+  defp merge_ranges([{start, end_pos} | rest], [{acc_start, acc_end} | acc_tail]) do
+    cond do
+      start <= acc_end ->
+        merge_ranges(rest, [{acc_start, max(end_pos, acc_end)} | acc_tail])
+
+      true ->
+        merge_ranges(rest, [{start, end_pos}, {acc_start, acc_end} | acc_tail])
+    end
   end
 end
 
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -2,15 +2,24 @@ defmodule Day05Test do
   use ExUnit.Case
 
   @example_input """
+  3-5
+  10-14
+  16-20
+  12-18
+
+  1
+  5
+  8
+  11
+  17
+  32
   """
 
-  @tag :skip
   test "solves example input for part 1" do
-    assert Day05.Part1.solve(@example_input) == 35
+    assert Day05.Part1.solve(@example_input) == 3
   end
 
-  @tag :skip
   test "solves example input for part 2" do
-    assert Day05.Part2.solve(@example_input) == 42
+    assert Day05.Part2.solve(@example_input) == 14
   end
 end
